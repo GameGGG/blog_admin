7c0663f5282454519ba9e5c3fb7c428c43a46205
StopllL <mganxiaolei@gmail.com>
2017-07-27 14:27:14 +0800

    'commit'

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').Server(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	mysql = require('mysql');
+	
 server.listen(80);
+
+
+// db connect
+var connection = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	password:'admin',
+	prot:'3306',
+	database:'passport'
+})
 
 
 app.use(express.static('www'));
@@ -14,6 +26,17 @@ io.sockets.on('connection',function(socket){
 	})
 })
 
+app.post('user/register',function(req,res){
+	let value_uname = req.query.uname,
+		value_password = req.query.password,
+		value_email = req.query.email;
+	if(!(vaule_uname && value_password && value_email)){
+		res.json({
+			state:0,
+			message:'缺少参数'
+		})
+	}
+})
 
 app.use(logErrors);
 app.use(errorHandler);
