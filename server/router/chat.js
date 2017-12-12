bce70466c49178ad1779b0ba0457e62453929cb1
StopllL <mganxiaolei@gmail.com>
2017-12-12 18:18:05 +0800

    websocket connection

--- server/router/chat.js
+++ server/router/chat.js
@@ -1,5 +1,7 @@
 const server = require('socket.io')
-
+const nickName = {}
+const nameUsed = []
+let nameNum = 1
 class Chat {
 	constructor(httpServer, options) {
 		this.init(httpServer, options)
@@ -9,10 +11,31 @@ class Chat {
 		io.on('connection', this.connect.bind(this))
 	}
 	connect (socket) {
-		console.log('socket connection is success')
+		const name = this.assetNickName(socket)
+		socket.emit('message', {
+			type: 'name',
+			body: name
+		})
+		this.joinRoom(socket, 'Total')
+		socket.on('message', (data) => {
+			console.log(data)
+		})
+	}
+	assetNickName (socket) {
+		let name = 'D' + nameNum
+		nameNum ++;
+		nickName[socket.id] = name
+		return name
+	}
+	joinRoom (socket, roomName) {
+		socket.join(roomName, () => {
+			socket.broadcast.to(roomName).emit('message',{
+				type: 'broadcast',
+				body: nickName[socket.id] + '加入聊天室' + roomName
+			})
+		})
 	}
 }
-
 module.exports = Chat
 
 
